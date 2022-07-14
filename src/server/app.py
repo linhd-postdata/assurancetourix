26e596d6cda5d651577bd3edb319bf761be7926b
Alejandro Benito-Santos <abenito@usal.es>
2022-07-14 09:46:19 +0200

    Enable reload in Flask debug mode

--- src/server/app.py
+++ src/server/app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+import rantanplan 
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, This is running from Docker!'
+def hello_rantanplan():
+    return f"Rantanplan version {rantanplan.__version__}" 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
