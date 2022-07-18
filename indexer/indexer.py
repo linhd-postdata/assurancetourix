6a1a1174f006b872df3c003a33fc86f7b7d373b0
Alejandro Benito-Santos <abenito@usal.es>
2022-07-18 16:45:55 +0200

    Add indexer container and script
    Corpora are downloaded using averell upon building the docker image.

--- indexer/indexer.py
+++ indexer/indexer.py
@@ -1,7 +1,30 @@
 from retry import retry
-import elasticsearch
-import elasticsearch.helpers
+from elasticsearch import Elasticsearch, helpers, ConnectionError
+from glob import glob
+import json
 
-print("Hello from the man")
+corpora_dir = '../corpora'
 
+@retry(ConnectionError, max_delay=300, delay=5)
+def indexer():
+    print("Trying to connect to ElasticSearch...")
+    es_client = Elasticsearch(
+        hosts=[{"host": "es-container", "port": 9200, "scheme": "http"}]
+    )
+    return es_client
 
+def load_json_corpora(dir):
+    pathname = dir + "/**/*.json"
+    print(pathname)
+    json_filenames = glob(pathname, recursive=True)
+    print(len(json_filenames))
+    for filename in json_filenames:
+        if 'averell' in filename:
+            with open(filename, 'r') as open_file:
+                yield json.load(open_file)
+
+if __name__ == "__main__":
+    es_client = indexer()
+    print("Connected to ElasticSearch!")
+    helpers.bulk(es_client, load_json_corpora(corpora_dir), index='corpora')
+    
